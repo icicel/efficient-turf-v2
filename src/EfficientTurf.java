ada188f2771dc053653887a4d6f489d1a57e9ac3
icicel <isakham@chalmers.se>
2023-08-25 03:08:48 +0200

    KML imports
    I hate Google btw

--- src/EfficientTurf.java
+++ src/EfficientTurf.java
@@ -1,8 +1,37 @@
+import java.io.*;
+import java.nio.file.*;
+import java.net.*;
+import java.net.http.*;
+import java.net.http.HttpResponse.BodyHandlers;
+
 public class EfficientTurf {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception {
+        return;
 
         // use depth first search with a single path object
         // if it's valid and finished then copy and save
+    }
+
+    // Get a KML file located in the root directory
+    public static String getKML(String file) throws IOException {
+        Path path = FileSystems.getDefault().getPath(".", file);
+        byte[] encoded = Files.readAllBytes(path);
+        return new String(encoded);
+    }
+
+    // Getting a KML by http request and a Google My Maps "mid" (map id)
+    // Not working yet
+    // Will probably have to pretend to be Firefox to get it to work
+    public static String getWebKML(String mapId) throws IOException, InterruptedException {
+        String urlString = "http://www.google.com/maps/d/kml?forcekml=1&mid=" + mapId;
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(urlString))
+            .build();
+        HttpResponse<String> response = 
+            client.send(request, BodyHandlers.ofString());
+        
+        return response.body();
     }
 }
