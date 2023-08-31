c383c9b7f5fbd19b7217020d83be2e06866e4f01
icicel <isakham@chalmers.se>
2023-08-31 14:52:04 +0200

    Allow to get a KML as a String

--- src/KML.java
+++ src/KML.java
@@ -4,16 +4,26 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 // Stores a KML InputStream
+// It actually doesn't have to be a KML
 public class KML {
 
-    InputStream stream;
+    private InputStream stream;
 
     public KML(Path path) throws IOException {
         this.stream = Files.newInputStream(path);
     }
-
     public KML(InputStream stream) {
         this.stream = stream;
     }
+
+    public InputStream asStream() {
+        return stream;
+    }
+
+    public String asString() throws IOException {
+        byte[] bytes = stream.readAllBytes();
+        return new String(bytes);
+    }
+
 
 }
--- test/Test.java
+++ test/Test.java
@@ -1,9 +1,9 @@
 public class Test {
     public static void main(String[] args) throws Exception {
-        String localKml = EfficientTurf.getKML("example.kml");
-        System.out.println(localKml.length());
-        String webKml = EfficientTurf.getWebKML("1sXlGZ7zc6IotVtMBEAtaHlTA4Uq2iA6q");
-        System.out.println(webKml.length());
+        KML localKml = EfficientTurf.getKML("example.kml");
+        System.out.println(localKml.asString().length());
+        KML webKml = EfficientTurf.getWebKML("1sXlGZ7zc6IotVtMBEAtaHlTA4Uq2iA6q");
+        System.out.println(webKml.asString().length());
 
         KMLParser kml = new KMLParser(localKml);
     }
