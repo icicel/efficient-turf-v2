eb7ceba72935415f744cf5c79406caac317130e4
icicel <isakham@chalmers.se>
2023-09-13 23:46:35 +0200

    Completed KMLParser methods

--- src/KMLHandler.java
+++ src/KMLHandler.java
@@ -1,17 +1,42 @@
+import java.util.HashSet;
+import java.util.Set;
 import org.xml.sax.Attributes;
 import org.xml.sax.helpers.DefaultHandler;
 
+// This class's methods are called by an XMLReader parsing a KML whenever certain
+//  events occur, such as at the start or end of a tag (element)
+// Google My Maps-exported KMLs use a <Folder> tag to store each layer
+// Objects in layers are stored in <Placemark> tags in these folders that contain
+//  either <Point> (for Zones) or <LineString> (for Connections)
+// Both <Folder> and <Placemark> tags also contain <name> tags
 public class KMLHandler extends DefaultHandler {
 
-    KMLParser parent;
+    // If targetLayer is null, all layers are parsed
+    private String targetLayer;
 
-    public KMLHandler(KMLParser parent) {
-        this.parent = parent;
+    private Set<Zone> zones;
+    private Set<Connection> connections;
+
+    public KMLHandler() {
+        resetStorage();
     }
 
-    // Returns information to parent
-    public void getEvents() {
-        return;
+    // Clears parsed zones and connections
+    public void resetStorage() {
+        zones = new HashSet<>();
+        connections = new HashSet<>();
+    }
+
+    // A KMLParser uses these methods to get the results of a parse
+    public Set<Zone> getZones() {
+        return zones;
+    }
+    public Set<Connection> getConnections() {
+        return connections;
+    }
+    
+    public void setTargetLayer(String targetLayer) {
+        this.targetLayer = targetLayer;
     }
 
     @Override
--- src/KMLParser.java
+++ src/KMLParser.java
@@ -11,36 +11,37 @@ public class KMLParser {
 
     private XMLReader xmlReader;
     private KML kml;
-
-    // Google My Maps-exported KMLs use a <Folder> tag containing a <name> tag to store layers
-    // Objects in layers are stored in <Placemark> tags in these folders
+    private KMLHandler handler;
     
     // Stores an XMLReader object to do the reading, and sets a KMLHandler
     //  linked to itself as both content and error handler
     public KMLParser(KML kml) throws ParserConfigurationException, SAXException, IOException {
         this.kml = kml;
         this.xmlReader = SAXParserFactory.newInstance().newSAXParser().getXMLReader();
+        this.handler = new KMLHandler();
 
-        KMLHandler handler = new KMLHandler(this);
         xmlReader.setContentHandler(handler);
         xmlReader.setErrorHandler(handler);
     }
 
     // Can be used as an empty parse to debug a KMLHandler
-    public void parse() throws IOException, SAXException {
+    // Clears the KMLHandler's storage first
+    // A new InputSource is created each time because the old one is closed after use
+    public void parse(String layerName) throws IOException, SAXException {
+        handler.setTargetLayer(layerName);
+        handler.resetStorage();
         InputSource kmlSource = new InputSource(kml.asReader());
         xmlReader.parse(kmlSource);
     }
 
-    // Get all zones in a layer
+    // Get all zones or connections in layerName
+    // Submit null as layerName to parse every layer
     public Set<Zone> getZones(String layerName) throws IOException, SAXException {
-        parse();
-        return null;
+        parse(layerName);
+        return handler.getZones();
     }
-
-    // Get all connections in a layer
     public Set<Connection> getConnections(String layerName) throws IOException, SAXException {
-        parse();
-        return null;
+        parse(layerName);
+        return handler.getConnections();
     }
 }
