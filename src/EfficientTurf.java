8c308a55880b97477a1bc5a3dea8196e9881c44c
icicel <isakham@chalmers.se>
2023-09-23 15:11:34 +0200

    Gave up on the KML automation dream :(
    Turned out to be pretty much impossible because Google has security measures put in place that caused me a lot of trouble. Maybe I will return to the task in the future, but I'll leave it at this for now

--- src/EfficientTurf.java
+++ src/EfficientTurf.java
@@ -1,9 +1,4 @@
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import kml.KML;
@@ -20,20 +15,5 @@ public class EfficientTurf {
     public static KML getKML(String file) throws IOException {
         Path path = FileSystems.getDefault().getPath(".", file);
         return new KML(path);
-    }
-
-    // Get a KML by http request and a Google My Maps "mid" (map id)
-    // Not working yet, will probably have to pretend to be Firefox to get it to work
-    public static KML getWebKML(String mapId) throws IOException, InterruptedException {
-        String urlString = "http://www.google.com/maps/d/kml?forcekml=1&mid=" + mapId;
-
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create(urlString))
-            .build();
-        HttpResponse<String> response = 
-            client.send(request, BodyHandlers.ofString());
-        
-        return new KML(response.body());
     }
 }
--- test/Test.java
+++ test/Test.java
@@ -7,10 +7,6 @@ import turf.Zone;
 public class Test {
     public static void main(String[] args) throws Exception {
         KML localKml = EfficientTurf.getKML("example.kml");
-        System.out.println(localKml.asString().length());
-        KML webKml = EfficientTurf.getWebKML("1sXlGZ7zc6IotVtMBEAtaHlTA4Uq2iA6q");
-        System.out.println(webKml.asString().length());
-
         KMLParser kml = new KMLParser(localKml);
         Set<Zone> zones = kml.getZones("Zones");
         Set<Zone> crossings = kml.getZones("Crossings");
