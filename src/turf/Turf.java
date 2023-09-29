4280a5b3ded53f12586d0236ec484a56056bd20d
icicel <isakham@chalmers.se>
2023-09-29 20:42:53 +0200

    No more EfficientTurf!

---
+++ src/turf/Turf.java
@@ -0,0 +1,39 @@
+package turf;
+import java.io.IOException;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
+import kml.KML;
+
+// Represents a collection of real zones, user-defined zones (optionally), and connections between them
+//   all extracted from the given KML file
+public class Turf {
+    
+    public ZoneSet zones;
+    public ConnectionSet connections;
+
+    // Give layer names of layers containing real zones, crossings, and connections for the
+    //   program to search through
+    // Set a layer name to null to search through all layers
+    public Turf(String kmlFile, String realZoneLayer, String crossingLayer, String connectionLayer)
+    throws IOException, InterruptedException, SAXException, ParserConfigurationException {
+        KML kml = new KML(kmlFile);
+        ZoneSet realZones = kml.getZones(realZoneLayer);
+        realZones.initPoints();
+        ZoneSet crossings = kml.getZones(crossingLayer);
+        this.zones = ZoneSet.union(realZones, crossings);
+
+        this.connections = kml.getConnections(connectionLayer, zones);
+    }
+    
+    // Give layer names of layers containing real zones and connections for the
+    //   program to search through
+    // Set a layer name to null to search through all layers
+    public Turf(String kmlFile, String zoneLayer, String connectionLayer)
+    throws IOException, InterruptedException, SAXException, ParserConfigurationException {
+        KML kml = new KML(kmlFile);
+        this.zones = kml.getZones(zoneLayer);
+        zones.initPoints();
+
+        this.connections = kml.getConnections(connectionLayer, zones);
+    }
+}
--- test/Test.java
+++ test/Test.java
@@ -1,27 +1,15 @@
-import kml.KML;
 import map.Connection;
 import map.Zone;
-import turf.ConnectionSet;
-import turf.ZoneSet;
+import turf.Turf;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-        KML kml = new KML("example.kml");
-        ZoneSet realZones = kml.getZones("Zones");
-        ZoneSet crossings = kml.getZones("Crossings");
-        ZoneSet allZones = ZoneSet.union(realZones, crossings);
-        ConnectionSet connections = kml.getConnections("Connections", allZones);
-
-        realZones.initPoints();
-
-        System.out.println(realZones.size());
-        System.out.println(crossings.size());
-        System.out.println(connections.size());
+        Turf turf = new Turf("example.kml", "Zones", "Crossings", "Connections");
         
-        for (Zone zone : allZones) {
+        for (Zone zone : turf.zones) {
             System.out.println(zone + " " + zone.points);
         }
-        for (Connection connection : connections) {
+        for (Connection connection : turf.connections) {
             System.out.println(connection + " " + connection.distance);
         }
     }
