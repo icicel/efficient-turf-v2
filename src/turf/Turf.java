e8d65c6905fad003f4053af84a11464224a3bd60
icicel <isakham@chalmers.se>
2023-09-30 01:09:18 +0200

    Added settings functionality

--- src/turf/Turf.java
+++ src/turf/Turf.java
@@ -4,6 +4,7 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.xml.sax.SAXException;
 import kml.KML;
 import zone.ConnectionSet;
+import zone.Zone;
 import zone.ZoneSet;
 
 // Represents a collection of real zones, user-defined zones (optionally), and connections between them
@@ -13,10 +14,21 @@ public class Turf {
     public ZoneSet zones;
     public ConnectionSet connections;
 
+    public double timeLimit;
+    public Zone startZone;
+    public Zone endZone;
+
+    public Zone[] blacklist;
+    public Zone[] whitelist;
+
+    public String username;
+    public double speed;
+    public double waitTime;
+
     // Give layer names of layers containing real zones, crossings, and connections for the
     //   program to search through
     // Set a layer name to null to search through all layers
-    public Turf(String kmlFile, String realZoneLayer, String crossingLayer, String connectionLayer)
+    public Turf(TurfSettings settings, String kmlFile, String realZoneLayer, String crossingLayer, String connectionLayer)
     throws IOException, InterruptedException, SAXException, ParserConfigurationException {
         KML kml = new KML(kmlFile);
         ZoneSet realZones = kml.getZones(realZoneLayer);
@@ -25,17 +37,53 @@ public class Turf {
         this.zones = ZoneSet.union(realZones, crossings);
 
         this.connections = kml.getConnections(connectionLayer, zones);
+
+        applySettings(settings);
     }
     
     // Give layer names of layers containing real zones and connections for the
     //   program to search through
     // Set a layer name to null to search through all layers
-    public Turf(String kmlFile, String zoneLayer, String connectionLayer)
+    public Turf(TurfSettings settings, String kmlFile, String zoneLayer, String connectionLayer)
     throws IOException, InterruptedException, SAXException, ParserConfigurationException {
         KML kml = new KML(kmlFile);
         this.zones = kml.getZones(zoneLayer);
         zones.initPoints();
 
         this.connections = kml.getConnections(connectionLayer, zones);
+
+        applySettings(settings);
+    }
+
+    private void applySettings(TurfSettings settings) {
+        // Check for undefined settings
+        if (settings.timeLimit == null) throw new IllegalArgumentException("Time limit undefined");
+        if (settings.startZone == null) throw new IllegalArgumentException("Start zone undefined");
+        if (settings.endZone == null) throw new IllegalArgumentException("End zone undefined");
+
+        // Apply settings
+        this.timeLimit = settings.timeLimit;
+        this.startZone = zones.findByName(settings.startZone);
+        this.endZone = zones.findByName(settings.endZone);
+
+        if (settings.blacklist != null) {
+            this.whitelist = namesToZones(settings.blacklist);
+        }
+        if (settings.whitelist != null) {
+            this.whitelist = namesToZones(settings.whitelist);
+        }
+
+        this.username = settings.username;
+        this.speed = settings.speed;
+        this.waitTime = settings.waitTime;
+    }
+
+    // Convert an array of zone names to an array of corresponding Zone objects
+    private Zone[] namesToZones(String[] names) {
+        Zone[] zones = new Zone[names.length];
+        for (int i = 0; i < names.length; i++) {
+            zones[i] = this.zones.findByName(names[i]);
+        }
+        return zones;
     }
 }
--- test/Test.java
+++ test/Test.java
@@ -1,10 +1,24 @@
 import turf.Turf;
+import turf.TurfSettings;
 import zone.Connection;
 import zone.Zone;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-        Turf turf = new Turf("example.kml", "Zones", "Crossings", "Connections");
+        // settings
+        TurfSettings settings = new TurfSettings()
+            .setTimeLimit(60.0)
+            .setStartZone("k-klassrum")
+            .setEndZone("k-nösnäs")
+            .setUsername("icicle")
+            .setSpeed(64.0)
+            .setWaitTime(1.0)
+        ;
+        Turf turf = new Turf(
+            settings, 
+            "example.kml", 
+            "Zones", "Crossings", "Connections"
+        );
         
         for (Zone zone : turf.zones) {
             System.out.println(zone + " " + zone.points);
