20599b3fd8e7bdeb7b515720a0e7874195a58835
icicel <isakham@chalmers.se>
2023-10-04 13:54:25 +0200

    ZoneSet tweaks/refactors

--- src/zone/ZoneSet.java
+++ src/zone/ZoneSet.java
@@ -15,14 +15,10 @@ import map.Coords;
 // Represents a set of Zones
 public class ZoneSet extends AbstractSet<Zone> {
 
-    public ZoneType type;
-
     private Map<String, Zone> nameMap;
 
-    // Similar to Zone class, type starts at CROSSING until initPoints() is called
     public ZoneSet(Set<Zone> zones) {
         this.set = zones;
-        this.type = ZoneType.CROSSING;
 
         // Build name map
         this.nameMap = new HashMap<>();
@@ -39,61 +35,57 @@ public class ZoneSet extends AbstractSet<Zone> {
         
         // Create a JSON body with all zone names
         // [{"name": "zonea"}, {"name": "zoneb"}, ...]
-        JSONArray json = new JSONArray();
+        JSONArray requestJson = new JSONArray();
         for (Zone zone : set) {
             JSONObject object = new JSONObject();
             object.put("name", zone.name);
-            json.put(object);
+            requestJson.put(object);
         }
 
         // Post the JSON to the API and get a response containing zone information
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
             .uri(URI.create("https://api.turfgame.com/v4/zones"))
-            .POST(HttpRequest.BodyPublishers.ofString(json.toString()))
+            .POST(HttpRequest.BodyPublishers.ofString(requestJson.toString()))
             .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        JSONArray zoneJson = new JSONArray(response.body());
+        JSONArray resultJson = new JSONArray(response.body());
 
         // Check that the response contains the same number of zones as the request
         // Should always be less - the API will ignore nonexistant zones
         // If not, tries to find those nonexistant zones
-        if (zoneJson.length() != set.size()) {
+        if (resultJson.length() != set.size()) {
             System.out.println("WARNING: API response contains less zones than requested");
-            findFakeZones(zoneJson);
+            findFakeZones(resultJson);
         }
 
         // Set points values to Zone objects
-        for (int i = 0; i < zoneJson.length(); i++) {
-            JSONObject zoneInfo = zoneJson.getJSONObject(i);
+        for (int i = 0; i < resultJson.length(); i++) {
+            JSONObject zoneInfo = resultJson.getJSONObject(i);
             String name = zoneInfo.getString("name").toLowerCase();
             Zone zone = findByName(name);
             zone.setPoints(zoneInfo, username);
         }
-
-        this.type = ZoneType.REAL;
     }
 
     // Find Zones that aren't real, a.k.a. don't exist in the API
-    private void findFakeZones(JSONArray zoneJson) {
+    private void findFakeZones(JSONArray resultJson) {
         Set<Zone> foundZones = new HashSet<>();
-        for (int i = 0; i < zoneJson.length(); i++) {
-            JSONObject zoneInfo = zoneJson.getJSONObject(i);
+        for (int i = 0; i < resultJson.length(); i++) {
+            JSONObject zoneInfo = resultJson.getJSONObject(i);
             String name = zoneInfo.getString("name").toLowerCase();
             Zone zone = findByName(name);
-            if (zone != null) {
-                foundZones.add(zone);
-            }
+            foundZones.add(zone);
         }
-        boolean fakeZone = false;
+        boolean foundFakeZone = false;
         for (Zone zone : set) {
             if (!foundZones.contains(zone)) {
                 System.out.println("ERROR: Zone " + zone.name + " does not exist in the API");
-                fakeZone = true;
+                foundFakeZone = true;
             }
         }
-        if (fakeZone) {
+        if (foundFakeZone) {
             throw new RuntimeException("Found fake zones");
         }
     }
