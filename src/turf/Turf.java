2bb0d3d921639518d63c220901642244cc6dce33
icicel <isakham@chalmers.se>
2023-10-10 15:08:05 +0200

    Have settings be configured in the Turf class again

--- src/turf/Turf.java
+++ src/turf/Turf.java
@@ -14,75 +14,67 @@ public class Turf {
     public ZoneSet zones;
     public ConnectionSet connections;
 
-    public double timeLimit;
-    public Zone startZone;
-    public Zone endZone;
-
+    // Zones that should not be visited
     public Zone[] blacklist;
+
+    // Zones that must be visited
     public Zone[] whitelist;
 
-    public String username;
-    public double speed;
-    public double waitTime;
+    // Speed in meters per minute (ignoring wait times at zones)
+    public double speed = 60.0;
 
+    // Time in minutes to wait at each zone
+    public double waitTime = 1.0;
+
+    // Initialize zones and connections from the given KML file
+    // Username is required but can be set to null
     // Give layer names of layers containing real zones, crossings, and connections for the
     //   program to search through
+    // Crossings are optional, set to null to ignore them
     // Set a layer name to null to search through all layers
-    public Turf(TurfSettings settings, String kmlFile, String realZoneLayer, String crossingLayer, String connectionLayer)
+    public Turf(String username, String kmlFile, String realZoneLayer, String crossingLayer, String connectionLayer)
     throws IOException, InterruptedException, SAXException, ParserConfigurationException {
         KML kml = new KML(kmlFile);
         ZoneSet realZones = kml.getZones(realZoneLayer);
-        realZones.initPoints(settings.username);
+        realZones.initPoints(username);
+
+        if (crossingLayer == null) {
+            this.zones = realZones;
+            this.connections = kml.getConnections(connectionLayer, zones);
+            return;
+        }
+
         ZoneSet crossings = kml.getZones(crossingLayer);
         this.zones = ZoneSet.union(realZones, crossings);
-
         this.connections = kml.getConnections(connectionLayer, zones);
-
-        applySettings(settings);
-    }
-    
-    // Give layer names of layers containing real zones and connections for the
-    //   program to search through
-    // Set a layer name to null to search through all layers
-    public Turf(TurfSettings settings, String kmlFile, String zoneLayer, String connectionLayer)
-    throws IOException, InterruptedException, SAXException, ParserConfigurationException {
-        KML kml = new KML(kmlFile);
-        this.zones = kml.getZones(zoneLayer);
-        zones.initPoints(settings.username);
-
-        this.connections = kml.getConnections(connectionLayer, zones);
-
-        applySettings(settings);
     }
 
-    private void applySettings(TurfSettings settings) {
-        // Check for undefined settings
-        if (settings.timeLimit == null) throw new IllegalArgumentException("Time limit undefined");
-        if (settings.startZone == null) throw new IllegalArgumentException("Start zone undefined");
-        if (settings.endZone == null) throw new IllegalArgumentException("End zone undefined");
+    /* Setters */
 
-        // Apply settings
-        this.timeLimit = settings.timeLimit;
-        this.startZone = zones.findByName(settings.startZone);
-        this.endZone = zones.findByName(settings.endZone);
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+    public void setWaitTime(double waitTime) {
+        this.waitTime = waitTime;
+    }
 
-        if (settings.blacklist != null) {
-            this.whitelist = namesToZones(settings.blacklist);
-        }
-        if (settings.whitelist != null) {
-            this.whitelist = namesToZones(settings.whitelist);
-        }
-
-        this.username = settings.username;
-        this.speed = settings.speed;
-        this.waitTime = settings.waitTime;
+    // List of names of zones 
+    public void setBlacklist(String[] blacklist) {
+        this.blacklist = namesToZones(blacklist);
+    }
+    public void setWhitelist(String[] whitelist) {
+        this.whitelist = namesToZones(whitelist);
     }
 
     // Convert an array of zone names to an array of corresponding Zone objects
     private Zone[] namesToZones(String[] names) {
+        if (names == null) {
+            return null;
+        }
         Zone[] zones = new Zone[names.length];
         for (int i = 0; i < names.length; i++) {
-            zones[i] = this.zones.findByName(names[i]);
+            String name = names[i].toLowerCase();
+            zones[i] = this.zones.findByName(name);
         }
         return zones;
     }
--- test/Test.java
+++ test/Test.java
@@ -1,24 +1,11 @@
 import turf.Turf;
-import turf.TurfSettings;
 import zone.Connection;
 import zone.Zone;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-        // settings
-        TurfSettings settings = new TurfSettings()
-            .setTimeLimit(60.0)
-            .setStartZone("k-klassrum")
-            .setEndZone("k-nösnäs")
-            .setUsername("icicle")
-            .setSpeed(64.0)
-            .setWaitTime(1.0)
-        ;
-        Turf turf = new Turf(
-            settings, 
-            "example.kml", 
-            "Zones", "Crossings", "Connections"
-        );
+        Turf turf = new Turf("icicle", "example.kml", "Zones", "Crossings", "Connections");
+        turf.setSpeed(64.0);
         
         for (Zone zone : turf.zones) {
             System.out.println(zone + " " + zone.points);
